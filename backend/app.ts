17f76adae8797e4576981ca8f6f863114ce2cd0e
Joschka Eickhoff <jeickhoff@posteo.de>
2024-09-13 13:00:51 +0200

    func / create flashcards

--- backend/app.ts
+++ backend/app.ts
@@ -1,17 +1,50 @@
-import express, { Express, Request, Response } from "express";
-import Post from "./src/model/postModel";
+import express, { Express, Request, RequestHandler, Response } from "express";
+import Flashcard from "./src/model/postModel";
 
-const app = express();
+const app: express.Application = express();
 
-app.get("/api/all", async (req: Request, res: Response) => {
-  const posts = await Post.find().exec();
-  console.log(posts);
+// enable express to send json
+app.use(express.json());
 
+// get all vocabs
+app.get("/api/vocabs", async (req: Request, res: Response) => {
+  const posts = await Flashcard.find();
   res.status(200).json(posts);
 });
 
-app.get("/hi", (req: Request, res: Response) => {
-  res.status(200).send("Hi!!!");
+// get vocab
+app.get("/api/vocabs/:vocabID", async (req: Request, res: Response) => {
+  const id = req.params.vocabID;
+  const targetPost = await Flashcard.findById(id);
+  res.status(200).json(targetPost);
 });
+
+/**
+ * CREATE VOCAB
+ */
+type CreateVocabBody = {
+  front_title?: string;
+  front_text?: string;
+  back_title?: string;
+  back_text?: string;
+};
+const createVocab: RequestHandler<unknown, unknown, CreateVocabBody, unknown> = async (req, res) => {
+  console.log(`\x1b[41m New Flashcard: \x1b[0m`);
+  console.log(req.body);
+
+  const { front_title, front_text, back_title, back_text } = req.body;
+
+  const newCard = await Flashcard.create({
+    front_title,
+    front_text,
+    back_title,
+    back_text,
+  });
+  res.status(200).json(newCard);
+
+  console.log(newCard);
+};
+
+app.post("/api/vocabs", createVocab);
 
 export default app;
