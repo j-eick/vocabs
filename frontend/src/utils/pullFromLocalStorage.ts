9b84312aff9ae1d146fe293396c407d3cb108bd5
Joschka Eickhoff <jeickhoff@posteo.de>
2024-09-24 17:05:30 +0200

    fix / pullFromLocalStorage

--- frontend/src/utils/pullFromLocalStorage.ts
+++ frontend/src/utils/pullFromLocalStorage.ts
@@ -1,15 +1,18 @@
 export default function pullFromLocalStorage(collection: string) {
   // check localstorage for cards
   try {
-    const pullFromLS = localStorage.getItem(collection);
-    if (pullFromLS) {
-      return JSON.parse(pullFromLS);
-    } else {
-      console.log("Collection doesn't exist");
-
-      return null;
+    const data = localStorage.getItem(collection);
+    // collection exists + array is NOT empty
+    if (data == null) {
+      throw new Error("Collection is null.");
+    } else if (!data.length) {
+      console.warn("Collection is empty!");
+      return [];
+    } else if (data) {
+      return JSON.parse(data);
     }
   } catch (err) {
     console.error(err);
+    return [];
   }
 }
