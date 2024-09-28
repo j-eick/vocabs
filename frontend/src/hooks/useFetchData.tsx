15251db9217dbeef7f3167046a66fcf5eb4e8fa8
Joschka Eickhoff <jeickhoff@posteo.de>
2024-09-28 23:41:18 +0200

    fix / replaced useState with Zustand + Immer

--- frontend/src/hooks/useFetchData.tsx
+++ frontend/src/hooks/useFetchData.tsx
@@ -1,41 +1,26 @@
 import { useEffect, useState } from "react";
-import { FlashcardProp } from "../types/flashcard";
 import * as FlashcardApi from "../network/flashcard_apis";
-import isNotNEU from "../utils/isNullOrEmpty";
+import useFlashcardsStore from "../store/flashcardStore";
 
-type UseFetchFromLSReturn = [FlashcardProp[], React.Dispatch<React.SetStateAction<FlashcardProp[]>>, boolean];
+type UseFetchFromLSReturn = [boolean];
 
 export default function useFetchData(): UseFetchFromLSReturn {
-  const [fetchedCards, setfetchedCards] = useState<FlashcardProp[]>([]);
   const [isLoading, setIsLoading] = useState<boolean>(false);
+  const saveToFlashcardstore = useFlashcardsStore((state) => state.saveToFlashcardStore);
+  const allflashcards = useFlashcardsStore((state) => state.allFlashcards);
 
   useEffect(() => {
-    fetchFlashcards();
+    if (!allflashcards.length) {
+      fetchFlashcards();
+    }
 
     async function fetchFlashcards() {
-      let localStorageData: FlashcardProp[] = []; // saved in localStorage
-      let mongoDbData: FlashcardProp[] = []; // saved in MongoDB
-
       try {
         setIsLoading(true);
 
-        // pull cards from LocalStorage
-        const cardsFromLS = localStorage.getItem("myCards");
-
-        // check for ls-collection "my cards"
-        if (isNotNEU(cardsFromLS)) {
-          console.log("a");
-          localStorageData = JSON.parse(cardsFromLS);
-          setfetchedCards(localStorageData);
-
-          // if not, fetch from MongoDB update localStorage and return fetched data
-        } else {
-          console.log("b");
-          const res = await FlashcardApi.fetchFlashcards();
-          mongoDbData = res;
-          localStorage.setItem("myCards", JSON.stringify(res));
-          setfetchedCards(mongoDbData);
-        }
+        console.log("Fetch from Zustand: (in-memory) => localStorage");
+        const res = await FlashcardApi.fetchFlashcards();
+        saveToFlashcardstore(res);
       } catch (err) {
         console.error(err);
       } finally {
@@ -44,5 +29,5 @@ export default function useFetchData(): UseFetchFromLSReturn {
     }
   }, []);
 
-  return [fetchedCards, setfetchedCards, isLoading];
+  return [isLoading];
 }
