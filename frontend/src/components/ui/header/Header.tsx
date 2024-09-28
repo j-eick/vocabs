0dbb4d50d0353d54f2138f054d6bfa4221cc3071
Joschka Eickhoff <jeickhoff@posteo.de>
2024-09-29 01:35:21 +0200

    created / Header.tsx

---
+++ frontend/src/components/ui/header/Header.tsx
@@ -0,0 +1,19 @@
+import { NavLink } from "react-router-dom";
+
+export default function Header() {
+  return (
+    <nav className="h-14 grid place-items-center bg-slate-200 ">
+      <ul className="w-full flex justify-evenly">
+        <li>
+          <NavLink to="/">Dashboard</NavLink>
+        </li>
+        <li>
+          <NavLink to="/study">Study</NavLink>
+        </li>
+        <li>
+          <NavLink to="/allVocabs">My Vocabs</NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
+}
