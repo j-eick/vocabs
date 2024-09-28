1976bd2076609ccd32c9a16d683fe9e2c0f35d9e
Joschka Eickhoff <jeickhoff@posteo.de>
2024-09-28 23:40:38 +0200

    using zustand on allVocabsPage.tsx

--- frontend/src/pages/allVocabs.tsx
+++ frontend/src/pages/allVocabs.tsx
@@ -1,10 +1,24 @@
 import { NavLink } from "react-router-dom";
+import useFlashcardsStore from "../store/flashcardStore";
+import { useEffect } from "react";
 
 export default function AllVocabsPage() {
+  const allFlashcards = useFlashcardsStore((state) => state.allFlashcards);
+
+  useEffect(() => {
+    if (allFlashcards) {
+      console.log(allFlashcards);
+    }
+  }, []);
   return (
     <>
       <NavLink to="/">go back</NavLink>
       <h1>AllVocabs</h1>
+      <ul>
+        {allFlashcards.map((card) => (
+          <li key={card._id}>yo!</li>
+        ))}
+      </ul>
     </>
   );
 }
