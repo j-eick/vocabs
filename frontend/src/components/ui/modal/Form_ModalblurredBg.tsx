fec9f0c89b1b9f43f78fa51c7f34eebe8a1d9748
Joschka Eickhoff <jeickhoff@posteo.de>
2024-10-01 12:27:11 +0200

    new / FormModal_blurredBg: Variant of Modal with blurred background that spans over screen

---
+++ frontend/src/components/ui/modal/Form_ModalblurredBg.tsx
@@ -0,0 +1,32 @@
+import { useEffect } from "react";
+import CreateNewVocab from "../form/CreateNewVocab";
+
+type ModalProps = {
+  show: boolean;
+  onClickOutside: () => void;
+};
+
+export default function FormModal_blurredBg({ onClickOutside, show }: ModalProps) {
+  useEffect(() => {
+    console.log(show);
+  });
+
+  return (
+    show && (
+      <div
+        className={`absolute inset-0 
+                  backdrop-blur-md backdrop-brightness-80`}
+      >
+        <div
+          className={`absolute w-4/5 min-h-44 mx-auto my-0 left-1/2 -translate-x-1/2 top-2/3 -translate-y-1/2 
+                    flex items-center justify-center 
+                    rounded-xl bg-zinc-200`}
+        >
+          <div className="pb-5">
+            <CreateNewVocab onClickOutside={onClickOutside} />
+          </div>
+        </div>
+      </div>
+    )
+  );
+}
