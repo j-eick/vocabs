92dce835d1a147d4cb1c6bd30971309e14a4fe76
Joschka Eickhoff <jeickhoff@posteo.de>
2024-10-01 12:29:40 +0200

    clean up

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,70 +5,9 @@ import Header from "./components/ui/header/Header";
 import { FlashcardProp } from "./types/flashcard";
 import FormModal_blurredBg from "./components/ui/modal/Form_ModalblurredBg";
 
-// type NewFlashcard = {
-//   front_title: string;
-//   front_text: string;
-//   back_title: string;
-//   back_text: string;
-// };
-
 export default function App() {
-  // const addToFlashcardStore = useFlashcardsStore((state) => state.addToFlashcardStore);
   const allFlashcards = useFlashcardsStore((state) => state.allFlashcards);
-  // const [newFlashcard, setNewFlashcard] = useState<NewFlashcard>({
-  //   front_title: "",
-  //   front_text: "",
-  //   back_title: "",
-  //   back_text: "",
-  // });
   const [showDialog, setShowDialog] = useState(false);
-  const [showCreateVocabButton, setShowCreateVocabButton] = useState(true);
-
-  // const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-  //   e.preventDefault();
-
-  //   // const formData = new FormData(e.currentTarget);
-  //   // const formObj = Object.fromEntries(formData.entries());
-
-  //   // console.log(formObj);
-
-  //   try {
-  //     const res = await fetch("http://localhost:3000/api/vocabs", {
-  //       method: "POST",
-  //       headers: {
-  //         "Content-Type": "application/json",
-  //       },
-  //       body: JSON.stringify(newFlashcard),
-  //     });
-
-  //     if (res.ok) {
-  //       const newCard = await res.json();
-  //       addToFlashcardStore(newCard);
-  //       // reset input field
-  //       setNewFlashcard({
-  //         front_title: "",
-  //         front_text: "",
-  //         back_title: "",
-  //         back_text: "",
-  //       });
-  //     }
-  //   } catch (err) {
-  //     console.error(err);
-  //   }
-  // };
-
-  // const inputFieldHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
-  //   const { name, value } = e.target;
-
-  //   setNewFlashcard((prev) => ({
-  //     ...prev,
-  //     [name]: value,
-  //   }));
-  // };
-
-  // useEffect(() => {
-  //   console.log(newFlashcard);
-  // });
 
   return (
     <main className="relative w-screen h-screen">
