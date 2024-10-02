f860ad4e0edd87c87f004adda0f79ad55efbb75d
Joschka Eickhoff <jeickhoff@posteo.de>
2024-10-02 16:37:05 +0200

    added react icons // added absolute bottom border for list items

--- frontend/src/components/ui/list-item-allVocabs/ListItem.tsx
+++ frontend/src/components/ui/list-item-allVocabs/ListItem.tsx
@@ -1,4 +1,5 @@
 import { FlashcardProp } from "../../../types/flashcard";
+import { MdOutlineDeleteOutline } from "react-icons/md";
 
 type ListItemProp = {
   flashcard: FlashcardProp;
@@ -7,10 +8,16 @@ type ListItemProp = {
 export default function ListItem({ flashcard }: ListItemProp) {
   const { front_title, back_title } = flashcard;
 
+  const removeVocabHandler = () => {};
+
   return (
-    <li className="grid grid-cols-12 gap-y-2 border-b border-slate-300">
-      <p className="col-span-4 ">{front_title}</p>
-      <p className="col-span-8">{back_title}</p>
-    </li>
+    <div className="relative">
+      <li className={`relative w-full h-8 grid grid-cols-12 gap-y-3 items-center`}>
+        <p className="col-span-4 ">{front_title}</p>
+        <p className="col-span-7">{back_title}</p>
+        <MdOutlineDeleteOutline className="col-span-1" onClick={removeVocabHandler} />
+      </li>
+      <div className="absolute border-b w-full" />
+    </div>
   );
 }
