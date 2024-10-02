c852fd3d05136cb234572bd4fed038b931f3c32f
Joschka Eickhoff <jeickhoff@posteo.de>
2024-10-03 00:12:12 +0200

    func / createFlashcard && deleteFlashcard

--- frontend/src/network/flashcard_apis.tsx
+++ frontend/src/network/flashcard_apis.tsx
@@ -1,5 +1,10 @@
 import { FlashcardProp } from "../types/flashcard";
+import { NewFlashcard } from "../components/ui/form/CreateNewVocab";
 
+/**
+ * GET ALL CARDS
+ * @returns updated JSON
+ */
 export const fetchFlashcards = async (): Promise<FlashcardProp[]> => {
   const res = await fetch("/api/vocabs", {
     method: "GET",
@@ -11,4 +16,36 @@ export const fetchFlashcards = async (): Promise<FlashcardProp[]> => {
   const result = await res.json();
 
   return result;
+};
+
+/**
+ * CREATE CARD
+ * @returns updated JSON
+ */
+export const createFlashcard = async (newFlashcard: NewFlashcard): Promise<FlashcardProp[]> => {
+  const res = await fetch("/api/vocabs", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(newFlashcard),
+  });
+
+  if (!res.ok) {
+    throw Error(`Error while fetching flashcards from DB: ${res}`);
+  }
+  const result = res.json();
+
+  return result;
+};
+
+/**
+ * DELETE CARD
+ * @param id
+ * @returns updated JSON
+ */
+export const deleteFlashcard = async (id: string) => {
+  await fetch(`/api/vocabs/${id}`, {
+    method: "DELETE",
+  });
 };
