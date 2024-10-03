239e534e96ebff680c885f5a1b92dff25c768c6b
Joschka Eickhoff <jeickhoff@posteo.de>
2024-10-03 21:56:51 +0200

    update / Flashcard v.0.2

--- frontend/src/components/ui/cardCaroussel/CardCaroussel.tsx
+++ frontend/src/components/ui/cardCaroussel/CardCaroussel.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { FlashcardProp } from "../../../types/flashcard";
+import { FaCaretRight } from "react-icons/fa6";
 
 type CardCarousselProps = {
   flashcards: FlashcardProp[];
@@ -7,13 +8,19 @@ type CardCarousselProps = {
 
 export default function CardCaroussel({ flashcards }: CardCarousselProps) {
   const [index, setIndex] = useState<number>(0);
+  const [back, setBack] = useState(false);
+
   if (!flashcards || !flashcards.length) {
     return <p>Flashcard couldn't be retrieved.</p>;
   }
   const { front_title, front_text, back_title, back_text, createdAt, updatedAt } = flashcards[index];
 
-  function handleClick() {
+  function handleSkipClick() {
     setIndex((prev) => (prev + 1) % flashcards.length);
+  }
+
+  function handleShowClick() {
+    setBack(!back);
   }
 
   // INFO: Flashcard Variants: (1) Space only for question (2) Indicates length of answer
@@ -22,18 +29,47 @@ export default function CardCaroussel({ flashcards }: CardCarousselProps) {
       {!flashcards.length || !flashcards ? (
         "Flashcard could not be retrieved."
       ) : (
-        <div
-          className={`min-h-40 py-3
+        <>
+          <div
+            className={`min-h-20 py-3 mb-10
                       shadow-line rounded-lg`}
-        >
-          <h1>{front_title}</h1>
-          <p>{front_text}</p>
-          <h1>{back_title}</h1>
-          <p>{back_text}</p>
-          <button onClick={handleClick} className="border-2 w-full">
-            next card
-          </button>
-        </div>
+          >
+            <h1>{front_title}</h1>
+            <p>{front_text}</p>
+          </div>
+          {back && (
+            <div
+              className={`min-h-20 py-3 mb-10
+                      shadow-line rounded-lg`}
+            >
+              <>
+                <h1>{back_title}</h1>
+                <p>{back_text}</p>
+              </>
+            </div>
+          )}
+          <div className="w-4/5 flex justify-evenly gap-5 mx-auto">
+            {!back ? (
+              <button className="w-20 p-2 rounded-2xl border-2" onClick={handleShowClick}>
+                show
+              </button>
+            ) : (
+              <button
+                className="w-20 p-2 rounded-2xl border-2"
+                onClick={() => {
+                  handleSkipClick();
+                  setBack(false);
+                }}
+              >
+                next
+              </button>
+            )}
+            <button className={`w-20 p-2 flex items-center gap-2 rounded-2xl border-2`} onClick={handleSkipClick}>
+              Skip
+              <FaCaretRight />
+            </button>
+          </div>
+        </>
       )}
     </>
   );
