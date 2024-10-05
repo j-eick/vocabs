9cce462255c5a8593255c304853686c11c4d5711
Joschka Eickhoff <jeickhoff@posteo.de>
2024-10-05 23:23:10 +0200

    prepared / UserModel

---
+++ backend/src/model/userModel.ts
@@ -0,0 +1,14 @@
+// // models/User.ts
+// import { Schema, model, Document, Types, InferSchemaType } from "mongoose";
+// import { StackModel } from "./stack";
+
+// type UserModel = InferSchemaType<typeof userSchema>;
+
+// const userSchema = new Schema({
+//   username: { type: String, required: true, unique: true },
+//   email: { type: String, required: true, unique: true },
+//   password: { type: String, required: true },
+//   stacks: [{ type: Schema.Types.ObjectId, ref: "Stack" }],
+// });
+
+// export default model<UserModel>("User", userSchema);
