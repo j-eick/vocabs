18a3d3301ed9c64c178bbaebae9b22a321ee4d7f
Joschka Eickhoff <jeickhoff@posteo.de>
2024-10-05 23:26:10 +0200

    update / save fetched data to new variable

--- frontend/src/hooks/useFetchData.tsx
+++ frontend/src/hooks/useFetchData.tsx
@@ -1,13 +1,17 @@
 import { useEffect, useState } from "react";
 import * as FlashcardApi from "../network/flashcard_apis";
+import * as StackApi from "../network/stackAPIs";
 import useFlashcardsStore from "../store/flashcardStore";
+import { StackProp } from "../types/stack";
 
 type UseFetchFromLSReturn = [boolean];
 
 export default function useFetchData(): UseFetchFromLSReturn {
   const [isLoading, setIsLoading] = useState<boolean>(false);
-  const saveToFlashcardstore = useFlashcardsStore((state) => state.saveToFlashcardStore);
+  // useStore
   const allflashcards = useFlashcardsStore((state) => state.allFlashcards);
+  const saveToFlashcardstore = useFlashcardsStore((state) => state.saveToFlashcardStore);
+  const savetoStore_allStackswithCards = useFlashcardsStore((state) => state.saveToAllStacksWithCards);
 
   useEffect(() => {
     if (!allflashcards.length) {
@@ -20,7 +24,10 @@ export default function useFetchData(): UseFetchFromLSReturn {
 
         console.log("Fetch from Zustand: (in-memory) => localStorage");
         const res = await FlashcardApi.fetchFlashcards();
+        const stacksAndCards: StackProp[] = await StackApi.fetchAllStacks();
+
         saveToFlashcardstore(res);
+        savetoStore_allStackswithCards(stacksAndCards);
       } catch (err) {
         console.error(err);
       } finally {
