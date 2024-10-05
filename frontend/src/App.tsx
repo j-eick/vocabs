3cbdb9add541c056ecc48a2caccdd6f07091e56f
Joschka Eickhoff <jeickhoff@posteo.de>
2024-10-05 23:27:44 +0200

    ui / displaying stacks for easier debugging

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,12 +4,18 @@ import { FlashcardProp } from "./types/flashcard";
 import FormModal_blurredBg from "./components/ui/modal/Form_ModalblurredBg";
 import useButtonStore from "./store/buttonStore";
 import useModalStore from "./store/modalStore";
+import { useEffect } from "react";
 
 export default function App() {
   const allFlashcards = useFlashcardsStore((state) => state.allFlashcards);
   const newVocabButton = useButtonStore((state) => state);
   const State_ModalNewVocabForm = useModalStore((state) => state.NewVocabFormModal_State);
   const showDialogModal = useModalStore((state) => state.ShowNewVocabModal);
+  const allStacksWithCards = useFlashcardsStore((state) => state.allStacksWithCards);
+
+  useEffect(() => {
+    console.log(allStacksWithCards);
+  });
 
   return (
     <main className="relative w-screen h-screen">
@@ -18,6 +24,16 @@ export default function App() {
       <section className="relative w-5/6 mx-auto my-0 mt-5">
         <LatestVocab flashcards={allFlashcards} />
       </section>
+      {allStacksWithCards && (
+        <ul>
+          {allStacksWithCards.map((stack, i) => (
+            <li key={i} className="border-2">
+              <p>{stack.name}</p>
+              <p>{`Number of flashcards: ${stack.flashcards.length}`}</p>
+            </li>
+          ))}
+        </ul>
+      )}
       <FormModal_blurredBg show={State_ModalNewVocabForm} onClickOutside={() => showDialogModal(false)} />
       {/* BUTTON: CREATE NEW VOCAB */}
       {newVocabButton.NewVocabButton_State && (
