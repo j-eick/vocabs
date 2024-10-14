e265690fcf2ba8fdd56a45602d4f26ad6165070f
Joschka Eickhoff <jeickhoff@posteo.de>
2024-10-14 15:07:41 +0200

    fix / clickOutsideHandler: nav hides when addButton clicked

--- frontend/src/utils/clickOutside.ts
+++ frontend/src/utils/clickOutside.ts
@@ -2,19 +2,19 @@ import { RefObject, useEffect } from "react";
 import useButtonStore from "../store/buttonStore";
 
 export const useClickOutside = (ref: RefObject<HTMLElement | undefined>, callback: () => void) => {
-  const showNewVocabButton = useButtonStore((state) => state.ShowNewVocabButton);
-  const handleClick = (event: MouseEvent) => {
-    if (ref.current && !ref.current.contains(event.target as HTMLElement)) {
-      callback();
-      showNewVocabButton(true);
-    }
-  };
-
-  useEffect(() => {
-    document.addEventListener("click", handleClick);
-
-    return () => {
-      document.removeEventListener("click", handleClick);
+    const { setAddFlashcardButton } = useButtonStore(state => state);
+    const handleClick = (event: MouseEvent) => {
+        if (ref.current && !ref.current.contains(event.target as HTMLElement)) {
+            callback();
+            setAddFlashcardButton(true);
+        }
     };
-  });
+
+    useEffect(() => {
+        document.addEventListener("click", handleClick);
+
+        return () => {
+            document.removeEventListener("click", handleClick);
+        };
+    });
 };
