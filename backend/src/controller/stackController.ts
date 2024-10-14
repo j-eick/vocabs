b2812bbbdf7363d01d61722937bfed1c667ef530
Joschka Eickhoff <jeickhoff@posteo.de>
2024-10-15 01:29:25 +0200

    ??

--- backend/src/controller/stackController.ts
+++ backend/src/controller/stackController.ts
@@ -8,28 +8,28 @@ import FlashcardModel from "../model/flashcardModel";
  * CREATE NEW STACK
  */
 export const createStack: RequestHandler = async (req, res) => {
-  try {
-    const { name, description } = req.body;
-    console.log(req.body);
+    try {
+        const { name, description } = req.body;
+        console.log(req.body);
 
-    // const userID = req.user.id;  // Assume `req.user` is populated by authentication middleware
+        // const userID = req.user.id;  // Assume `req.user` is populated by authentication middleware
 
-    // Validate required fields
-    if (!name || !description) {
-      return res.status(400).json({ message: "Name and description are required" });
+        // Validate required fields
+        if (!name || !description) {
+            return res.status(400).json({ message: "Name and description are required" });
+        }
+
+        const newStack = await StackModel.create({
+            name,
+            description,
+            flashcards: [],
+        });
+
+        res.status(201).json(newStack);
+    } catch (error) {
+        console.error("Server error while creating stack:" + error);
+        res.status(500).send("Server error");
     }
-
-    const newStack = await StackModel.create({
-      name,
-      description,
-      flashcards: [],
-    });
-
-    res.status(201).json(newStack);
-  } catch (error) {
-    console.error("Server error while creating stack:" + error);
-    res.status(500).send("Server error");
-  }
 };
 
 /**
@@ -44,57 +44,57 @@ export const createStack: RequestHandler = async (req, res) => {
 //   updatedAt: string;
 // }
 export const getStacks: RequestHandler = async (req, res) => {
-  let allStacks;
+    let allStacks;
 
-  try {
-    allStacks = await StackModel.find().populate("flashcards").exec();
+    try {
+        allStacks = await StackModel.find().populate("flashcards").exec();
 
-    res.status(200).json(allStacks);
-  } catch (error) {
-    console.error("Error while fetching Stacks from MongoDB " + error);
-    res.status(500).json({ message: "Server errror while fetching stacks" });
-  }
+        res.status(200).json(allStacks);
+    } catch (error) {
+        console.error("Error while fetching Stacks from MongoDB " + error);
+        res.status(500).json({ message: "Server errror while fetching stacks" });
+    }
 };
 
 /**
  * DELETE STACK
  */
 export const deleteStack: RequestHandler = async (req, res) => {
-  const targetID = req.params.stackID;
-  const withCards = req.query.withCards;
-  console.log(targetID);
-  console.log(withCards);
+    const targetID = req.params.stackID;
+    const withCards = req.query.withCards;
+    console.log(targetID);
+    console.log(withCards);
 
-  try {
-    // check validity of flashcard id
-    if (!mongoose.isValidObjectId(targetID)) {
-      throw new Error("Invalid Flashcard ID!");
+    try {
+        // check validity of flashcard id
+        if (!mongoose.isValidObjectId(targetID)) {
+            throw new Error("Invalid Flashcard ID!");
+        }
+
+        const targetStack = await StackModel.findById(targetID).exec();
+        if (!targetStack) {
+            throw new Error("Stack with this ID does not exist.");
+        }
+
+        if (withCards === "true") {
+            // DELETE STACK INCL. CARDS
+            await FlashcardModel.deleteMany({ stack: targetID });
+            await StackModel.findByIdAndDelete(targetID);
+        } else {
+            // DELETE STACK w/o CARDS
+            await StackModel.findByIdAndDelete(targetID);
+        }
+
+        res.sendStatus(204);
+        CC("Removed Stack", "warn");
+        console.log(`${targetID}`);
+    } catch (err) {
+        console.error(err);
+
+        if (err instanceof Error) {
+            res.status(500).json({ error: err.message });
+        } else {
+            res.status(500).json({ error: "An unknown error occurred." });
+        }
     }
-
-    const targetStack = await StackModel.findById(targetID).exec();
-    if (!targetStack) {
-      throw new Error("Stack with this ID does not exist.");
-    }
-
-    if (withCards === "true") {
-      // DELETE STACK INCL. CARDS
-      await FlashcardModel.deleteMany({ stack: targetID });
-      await StackModel.findByIdAndDelete(targetID);
-    } else {
-      // DELETE STACK w/o CARDS
-      await StackModel.findByIdAndDelete(targetID);
-    }
-
-    res.sendStatus(204);
-    CC("Removed Stack", "warn");
-    console.log(`${targetID}`);
-  } catch (err) {
-    console.error(err);
-
-    if (err instanceof Error) {
-      res.status(500).json({ error: err.message });
-    } else {
-      res.status(500).json({ error: "An unknown error occurred." });
-    }
-  }
 };
