fdac34ecf0059b2be979309e5fb5073e01d1ebf6
Joschka Eickhoff <jeickhoff@posteo.de>
2024-10-15 01:26:56 +0200

    fix / Dropdown.tsx

--- frontend/src/components/ui/dropdown-menu/Dropdown.tsx
+++ frontend/src/components/ui/dropdown-menu/Dropdown.tsx
@@ -1,37 +1,38 @@
-import { ChangeEvent } from "react";
+import { ChangeEvent, useEffect } from "react";
+import { StackProp } from "../../../types/stack";
 
-interface HasNameAndID {
-  name: string;
-  id: string;
-}
-
-type StacksOverviewProps<T extends HasNameAndID> = {
-  listItems: T[];
-  onChange: (e: ChangeEvent<HTMLSelectElement>) => void;
-  label?: string;
-  dropDownValue: string;
+type StacksOverviewProps = {
+    listItems: StackProp[];
+    onChange: (e: ChangeEvent<HTMLSelectElement>) => void;
+    label?: string;
+    value: string;
 };
 
-export default function Dropdown<T extends HasNameAndID>({
-  listItems,
-  onChange,
-  label,
-  dropDownValue,
-}: StacksOverviewProps<T>) {
-  console.log(listItems);
+export default function Dropdown({ listItems, onChange, label, value }: StacksOverviewProps) {
+    useEffect(() => {
+        console.log(listItems);
+    });
 
-  return (
-    <div className="flex flex-col">
-      {label && <label htmlFor="stackOverview">{label}</label>}
-      {listItems.length >= 1 && (
-        <select name="Stack-Overview" id="stackOverview" value={dropDownValue} onChange={onChange}>
-          {listItems.map((item, i) => (
-            <option key={i} value={item.id}>
-              {item.name}
-            </option>
-          ))}
-        </select>
-      )}
-    </div>
-  );
+    return (
+        <div className="flex flex-col">
+            {label && <label htmlFor="stackOverview">{label}</label>}
+            {listItems.length >= 1 && (
+                <select
+                    name="Stack-Overview"
+                    id="stackOverview"
+                    value={value}
+                    onChange={onChange}
+                >
+                    {listItems.map((item, i) => (
+                        <option
+                            key={i}
+                            value={item._id}
+                        >
+                            {item.name}
+                        </option>
+                    ))}
+                </select>
+            )}
+        </div>
+    );
 }
