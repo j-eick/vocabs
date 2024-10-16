1c28f100de46e171a758cfaa14dfcd4897298138
Joschka Eickhoff <jeickhoff@posteo.de>
2024-10-16 02:33:52 +0200

    func / new stackAPI: renameStack

--- frontend/src/network/stackAPIs.ts
+++ frontend/src/network/stackAPIs.ts
@@ -1,41 +1,63 @@
 export async function fetchAllStacks() {
-  let res;
+    let res;
 
-  try {
-    res = await fetch("api/stacks", { method: "GET" });
+    try {
+        res = await fetch("api/stacks", { method: "GET" });
 
-    if (res.ok) {
-      return await res.json();
-    } else {
-      throw new Error("Something went wrong while loading stacks.");
+        if (res.ok) {
+            return await res.json();
+        } else {
+            throw new Error("Something went wrong while loading stacks.");
+        }
+    } catch (error) {
+        console.error("Couldn't retrieve stacks: " + error);
     }
-  } catch (error) {
-    console.error("Couldn't retrieve stacks: " + error);
-  }
 }
 
 // TODO: createStack needs to be passed the newStack; need to create stack via form/dropdown
 export async function createStack() {
-  let res;
+    let res;
 
-  try {
-    res = await fetch("api/stacks", {
-      method: "POST",
-      headers: {
-        "Content-Type": "Application/Json",
-      },
-      body: JSON.stringify(res),
-    });
+    try {
+        res = await fetch("api/stacks", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(res),
+        });
 
-    if (res.ok) {
-      return await res.json();
-    } else {
-      throw new Error("Something went wrong while creating a new stack.");
+        if (res.ok) {
+            return await res.json();
+        } else {
+            throw new Error("Something went wrong while creating a new stack.");
+        }
+    } catch (error) {
+        console.error(error);
     }
-  } catch (error) {
-    console.error(error);
-  }
 }
+
+export const renameStack = async (stackID: string, newName: string) => {
+    let res;
+
+    try {
+        res = await fetch(`/api/stacks/${stackID}`, {
+            method: "PATCH",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ newName }),
+        });
+
+        if (!res) {
+            throw new Error("Errow while changing collection-name.");
+        }
+
+        return await res.json();
+    } catch (error) {
+        console.error(error);
+    }
+};
 
 /**
  * DELETE CARD
@@ -43,9 +65,9 @@ export async function createStack() {
  * @returns updated JSON
  */
 export const deleteStack = async (stackID: string) => {
-  await fetch(`/api/stacks/${stackID}`, {
-    method: "DELETE",
-  });
+    await fetch(`/api/stacks/${stackID}`, {
+        method: "DELETE",
+    });
 };
 
 /**
@@ -54,7 +76,7 @@ export const deleteStack = async (stackID: string) => {
  * @returns updated JSON
  */
 export const deleteStackWithCards = async (stackID: string, inclCards: boolean) => {
-  await fetch(`/api/stacks/${stackID}?withCards=${inclCards}`, {
-    method: "DELETE",
-  });
+    await fetch(`/api/stacks/${stackID}?withCards=${inclCards}`, {
+        method: "DELETE",
+    });
 };
