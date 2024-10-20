8d58a0929dba7840bd56f1276c36c4cc2c0df537
Joschka Eickhoff <jeickhoff@posteo.de>
2024-10-20 03:14:35 +0200

    update / added custom tailwind styling

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,21 +1,35 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{html,js,ts,tsx,jsx}"],
-  theme: {
-    extend: {
-      backdropBrightness: {
-        25: ".25",
-        80: ".8",
-      },
-      boxShadow: {
-        bottomRight: "rgba(0, 0, 0, 0.15) 1.95px 1.95px 2.6px",
-        line: "rgba(0, 0, 0, 0.02) 0px 1px 3px 0px, rgba(27, 31, 35, 0.15) 0px 0px 0px 1px",
-      },
+    content: ["./index.html", "./src/**/*.{html,js,ts,tsx,jsx}"],
+    theme: {
+        extend: {
+            backdropBrightness: {
+                25: ".25",
+                80: ".8",
+            },
+            blur: {
+                smm: "8px",
+                smmm: "10px",
+                kl: "14px",
+            },
+            boxShadow: {
+                bottomRight: "rgba(0, 0, 0, 0.15) 1.95px 1.95px 2.6px",
+                line: "rgba(0, 0, 0, 0.02) 0px 1px 3px 0px, rgba(27, 31, 35, 0.15) 0px 0px 0px 1px",
+            },
+            keyframes: {
+                fadeIn: {
+                    "0%": { opacity: "0%" },
+                    "100%": { opacity: "100%" },
+                },
+            },
+            animation: {
+                fadeIn: "fadeIn .25s ease-in-out",
+            },
+        },
     },
-  },
-  plugins: [require("daisyui")],
-  daisyui: {
-    // themes: ["light", "dark", "cupcake"],
-    themes: false,
-  },
+    plugins: [require("daisyui")],
+    daisyui: {
+        // themes: ["light", "dark", "cupcake"],
+        themes: false,
+    },
 };
