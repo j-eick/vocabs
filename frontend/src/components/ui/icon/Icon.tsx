0ba8c2039889f79de5410e517b9795053ba45b93
Joschka Eickhoff <jeickhoff@posteo.de>
2024-10-20 03:15:15 +0200

    update / Icon comp handles clicks

--- frontend/src/components/ui/icon/Icon.tsx
+++ frontend/src/components/ui/icon/Icon.tsx
@@ -3,7 +3,7 @@ import { IconType } from "react-icons";
 type IconProps = {
     icon: IconType;
     className?: string;
-    onClick?: (e: MouseEvent) => void;
+    onClick?: (e: React.MouseEvent<SVGElement>) => void;
 };
 
 export default function Icon({ icon: Icon, onClick, className }: IconProps) {
