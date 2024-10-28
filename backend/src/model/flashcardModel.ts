6001d0f207f76bf7bbba952387413bc8fcf505d3
Joschka Eickhoff <jeickhoff@posteo.de>
2024-10-28 15:07:34 +0100

    fix / updated flashcard & stack Models

--- backend/src/model/flashcardModel.ts
+++ backend/src/model/flashcardModel.ts
@@ -3,26 +3,36 @@ import mongoose, { InferSchemaType, Schema, model, models } from "mongoose";
 type Flashcard = InferSchemaType<typeof flashcardSchema>;
 
 const flashcardSchema = new Schema(
-  {
-    front_title: {
-      type: String,
-      required: true,
+    {
+        front_title: {
+            type: String,
+            required: true,
+        },
+        front_text: {
+            type: String,
+            default: "",
+        },
+        back_title: {
+            type: String,
+            default: "",
+        },
+        back_text: {
+            type: String,
+            default: "",
+        },
+        stack: { type: Schema.Types.ObjectId, ref: "Stack", required: true },
+        learnStatus: {
+            type: String,
+            enum: ["unsorted", "dontKnow", "notSure", "know"],
+            default: "unsorted",
+        },
+        lastReviewed: { type: Date },
+        nextReviewDate: { type: Date },
+        reviewInterval: { type: Number },
+        easinessFactor: { type: Number },
+        repetitionCount: { type: Number },
     },
-    front_text: {
-      type: String,
-      default: "",
-    },
-    back_title: {
-      type: String,
-      default: "",
-    },
-    back_text: {
-      type: String,
-      default: "",
-    },
-    stack: { type: Schema.Types.ObjectId, ref: "Stack", required: true },
-  },
-  { timestamps: true }
+    { timestamps: true }
 );
 
 export default model<Flashcard>("Flashcard", flashcardSchema);
--- backend/src/model/stackModel.ts
+++ backend/src/model/stackModel.ts
@@ -3,18 +3,18 @@ import { InferSchemaType, model, Schema } from "mongoose";
 export type StackModel = InferSchemaType<typeof stackSchema>;
 
 const stackSchema = new Schema(
-  {
-    name: { type: String, required: true },
-    description: { type: String },
-    // user: { type: Schema.Types.ObjectId, ref: "User", required: true },
-    flashcards: [
-      {
-        flashcard: { type: Schema.Types.ObjectId, ref: "Flashcard" },
-        category: { type: String, enum: ["beginner", "intermediate", "advanced"] },
-      },
-    ],
-  },
-  { timestamps: true }
+    {
+        name: { type: String, required: true },
+        description: { type: String },
+        // user: { type: Schema.Types.ObjectId, ref: "User", required: true },
+        flashcards: [
+            {
+                flashcard: { type: Schema.Types.ObjectId, ref: "Flashcard" },
+                // category: { type: String, enum: ["beginner", "intermediate", "advanced"] },
+            },
+        ],
+    },
+    { timestamps: true }
 );
 
 export default model<StackModel>("Stack", stackSchema);
