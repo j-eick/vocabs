d3381a07f196fd2915525181896aa78aee847cbf
Joschka Eickhoff <jeickhoff@posteo.de>
2024-10-29 17:52:26 +0100

    fix / CardCaroussel: improved structure

--- frontend/src/components/ui/cardCaroussel/CardCaroussel.tsx
+++ frontend/src/components/ui/cardCaroussel/CardCaroussel.tsx
@@ -1,76 +1,58 @@
 import { useState } from "react";
 import { FlashcardProp } from "../../../types/flashcard";
-import { FaCaretRight } from "react-icons/fa6";
+import Line from "../shapes/Line";
 
 type CardCarousselProps = {
-  flashcards: FlashcardProp[];
+    flashcards: FlashcardProp[];
+    className: string;
+    showBack: boolean;
 };
 
-export default function CardCaroussel({ flashcards }: CardCarousselProps) {
-  const [index, setIndex] = useState<number>(0);
-  const [back, setBack] = useState(false);
+export default function CardCaroussel({ flashcards, showBack, className }: CardCarousselProps) {
+    const [index, setIndex] = useState<number>(0);
+    if (!flashcards || !flashcards.length) {
+        return <p>Flashcard couldn't be retrieved.</p>;
+    }
 
-  if (!flashcards || !flashcards.length) {
-    return <p>Flashcard couldn't be retrieved.</p>;
-  }
-  const { front_title, front_text, back_title, back_text, createdAt, updatedAt } = flashcards[index];
+    const { front_title, front_text, back_title, back_text, createdAt, updatedAt } = flashcards[index];
 
-  function handleSkipClick() {
-    setIndex((prev) => (prev + 1) % flashcards.length);
-  }
+    function handleSkipClick() {
+        setIndex(prev => (prev + 1) % flashcards.length);
+    }
 
-  function handleShowClick() {
-    setBack(!back);
-  }
+    const spanFullCardHeight = "h-full grow grid place-content-center";
 
-  // INFO: Flashcard Variants: (1) Space only for question (2) Indicates length of answer
-  return (
-    <>
-      {!flashcards.length || !flashcards ? (
-        "Flashcard could not be retrieved."
-      ) : (
+    // INFO: Flashcard Variants: (1) Space only for question (2) Indicates length of answer
+    return (
         <>
-          <div
-            className={`min-h-20 py-3 mb-10
-                      shadow-line rounded-lg`}
-          >
-            <h1>{front_title}</h1>
-            <p>{front_text}</p>
-          </div>
-          {back && (
-            <div
-              className={`min-h-20 py-3 mb-10
-                      shadow-line rounded-lg`}
-            >
-              <>
-                <h1>{back_title}</h1>
-                <p>{back_text}</p>
-              </>
-            </div>
-          )}
-          <div className="w-4/5 flex justify-evenly gap-5 mx-auto">
-            {!back ? (
-              <button className="w-20 p-2 rounded-2xl border-2" onClick={handleShowClick}>
-                show
-              </button>
+            {!flashcards.length || !flashcards ? (
+                "Flashcard could not be retrieved."
             ) : (
-              <button
-                className="w-20 p-2 rounded-2xl border-2"
-                onClick={() => {
-                  handleSkipClick();
-                  setBack(false);
-                }}
-              >
-                next
-              </button>
+                <div
+                    className={`min-h-24 py-2 flex flex-col
+                                shadow-line rounded-lg overflow-hidden ${className}`}
+                >
+                    {/* FRONT-SIDE */}
+                    <div className={`py-2 ${!showBack ? spanFullCardHeight : ""}`}>
+                        <h1>{front_title}</h1>
+                        <p>{front_text}</p>
+                    </div>
+
+                    {/* BACK-SIDE */}
+                    <div className={`${!showBack ? "hidden" : "animate-fadeIn"}`}>
+                        <Line
+                            lineBox="h-6 grid place-items-center"
+                            lineStyle="w-5/6 border-t border-slate-400"
+                        />
+                        <div className="py-2">
+                            <div className={`${!showBack ? "hidden" : ""}`}>
+                                <h1>{back_title}</h1>
+                                <p>{back_text}</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             )}
-            <button className={`w-20 p-2 flex items-center gap-2 rounded-2xl border-2`} onClick={handleSkipClick}>
-              Skip
-              <FaCaretRight />
-            </button>
-          </div>
         </>
-      )}
-    </>
-  );
+    );
 }
