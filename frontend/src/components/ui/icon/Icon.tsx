ebee2779525aeffae188f6a2919d714960108635
Joschka Eickhoff <jeickhoff@posteo.de>
2024-10-29 17:50:45 +0100

    update / Icon component: takes either an icon as prop (e.g. react icon) or a Reactnode as children.

--- frontend/src/components/ui/icon/Icon.tsx
+++ frontend/src/components/ui/icon/Icon.tsx
@@ -1,16 +1,36 @@
 import { IconType } from "react-icons";
 
 type IconProps = {
-    icon: IconType;
+    icon?: IconType;
     className?: string;
-    onClick?: (e: React.MouseEvent<SVGElement>) => void;
+    onClick?: (e: React.MouseEvent<SVGElement | HTMLElement>) => void;
+    children?: string | null;
 };
 
-export default function Icon({ icon: Icon, onClick, className }: IconProps) {
-    return (
+/**
+ * Icon component
+ * - 2 ways to use this:
+ *   1. Pass a Reactnode as child: `<Icon> some text </Icon`
+ *   2. Pass a prop of type icon: `<Icon icon = { <AiFillAudio /> } />`
+ *
+ * @param icon
+ * @param className
+ * @param onCLick
+ * @param children
+ * @returns
+ */
+export default function Icon({ icon: Icon, children, onClick, className }: IconProps) {
+    return Icon ? (
         <Icon
             className={className}
             onClick={onClick}
         />
+    ) : (
+        <div
+            className={className}
+            onClick={onClick}
+        >
+            {children}
+        </div>
     );
 }
