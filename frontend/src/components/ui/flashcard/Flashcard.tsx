4806cb3136edb201c8d23f44c1b0940addf9ae6e
Joschka Eickhoff <jeickhoff@posteo.de>
2024-10-29 15:20:03 +0100

    not is use / Flashcard.tsx

--- frontend/src/components/ui/flashcard/Flashcard.tsx
+++ frontend/src/components/ui/flashcard/Flashcard.tsx
@@ -1,14 +1,14 @@
-import { FlashcardProp } from "../../../types/flashcard";
+// import { FlashcardProp } from "../../../types/flashcard";
 
-export function Flashcard({ flashcard }: { flashcard: FlashcardProp }) {
-  const { _id, front_title, front_text, back_title, back_text, createdAt, updatedAt } = flashcard;
+// export function Flashcard({ flashcard }: { flashcard: FlashcardProp }) {
+//     const { _id, front_title, front_text, back_title, back_text, createdAt, updatedAt } = flashcard;
 
-  return (
-    <div>
-      <h1>{front_title}</h1>
-      <p>{front_text}</p>
-      <h1>{back_title}</h1>
-      <p>{back_text}</p>
-    </div>
-  );
-}
+//     return (
+//         <div className="h-full">
+//             <h1>{front_title}</h1>
+//             <p>{front_text}</p>
+//             <h1>{back_title}asd</h1>
+//             <p>{back_text}</p>
+//         </div>
+//     );
+// }
